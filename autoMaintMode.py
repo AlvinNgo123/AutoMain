6c7387bd60f590df11ecbf00bb83c1f08b7fdf96
Alvin Ngo <aln011@ucsd.edu>
2021-07-02 15:46:17 -0700

    Add all basic functionality of comparing dates to turn on and off (renaming) when dates match

--- autoMaintMode.py
+++ autoMaintMode.py
@@ -8,7 +8,6 @@ import ast
 def getCurrentDateTimeList():
 	currentDateTime = datetime.now()
 	currDTstring = currentDateTime.strftime("%d/%m/%Y/%H/%M/%S")
-	print(currDTstring)
 	currDTlist = currDTstring.split('/')
 
 	currMonth = currDTlist[1]
@@ -22,13 +21,53 @@ def getCurrentDateTimeList():
 
 def logProgramRunning(todayDateTimeList):
 	fileLog = open("autoMaintLog.txt", "a")
-	#
+	#fileLog = open("/var/www/html/admin/autoMaintLog.txt", "a") 
 	dateLogLine = todayDateTimeList[0] + "/" + todayDateTimeList[1] + "/" + todayDateTimeList[2] 
 	timeLogLine = todayDateTimeList[3] + ":" + todayDateTimeList[4]  
 
 	fileLog.write(dateLogLine + " " + timeLogLine + " - Cron Scheduler was ran\n")
 	fileLog.close()
 
+def getAllDatesList(filename):
+	dateFile = open(filename, "r")
+
+	dateFileContents = dateFile.read()
+	allDatesList = ast.literal_eval(dateFileContents)
+
+	dateFile.close()
+	return allDatesList
+
+def switchMaintenanceMode(command):
+	'''if command == "off" and path.exists('htcaccess.on'):
+		os.rename('htcaccess.on','htcaccess.off')
+		#TODO: send email saying maintenance mode has been enabled
+	elif command == "on" and path.exists('htcaccess.off'):
+		os.rename('htcaccess.off','htcaccess.on')
+		#TODO: send email saying maintenance mode has been disabled'''
+
+	if command == "off" and path.exists('.htcaccess'):
+		os.rename('.htcaccess','.htcaccess.off')
+		#TODO: send email saying maintenance mode has been enabled
+	elif command == "on" and path.exists('.htcaccess.off'):
+		os.rename('.htcaccess.off','.htcaccess')
+		#TODO: send email saying maintenance mode has been disabled
+
+def compareDates(todayDateTimeList, allDatesList):
+	startList, endList = [], []
+	for dateList in allDatesList:
+		startList = [dateList['startMonth'], dateList['startDay'], dateList['startYear'], dateList['startHour'], dateList['startMinute']] 
+		endList = [dateList['endMonth'], dateList['endDay'], dateList['endYear'], dateList['endHour'], dateList['endMinute']]
+
+		#check to see if today/current time lines up with the dates/times in the list
+		if todayDateTimeList == startList: 
+			switchMaintenaceMode("on")
+			print("here")
+		elif todayDateTimeList == endList:
+			switchMaintenanceMode("off")
+			print("there")
+
 todayDateTimeList = getCurrentDateTimeList()
 logProgramRunning(todayDateTimeList)
-print(todayDateTimeList)
+allDatesList = getAllDatesList("allAutoTimes.txt")
+#allDatesList = getAllDatesList("/var/www/html/admin/allAutoTimes.txt")
+compareDates(todayDateTimeList, allDatesList)
